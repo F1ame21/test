ff9146b712390d4357313f162eb0c7d3885f41e5
Ivan <ipiskunov55@gmail.com>
2023-11-24 13:34:17 +0300

    firrst commit

---
+++ circle.py
@@ -0,0 +1,12 @@
+import math
+
+
+def area(r):
+    """ Возвращает площадь круга, радиуса r """ 
+    return math.pi * r * r
+
+
+def perimeter(r):
+    """ Возвращает периметр круга, радиуса r """ 
+    return 2 * math.pi * r
+
